733ea949f2148eef2b40fbf66086f8616f245f54
RiffTable <jarif.m.aabir@gmail.com>
2026-01-03 23:25:24 +0600

    Added minimize feature

--- .config/hypr/unminimize.sh
+++ .config/hypr/unminimize.sh
@@ -1,10 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
-hyprctl clients -j \
-| jq -r '.[] 
-  | select(.workspace.name == "special:magic")
-  | "\(.address)\t\(.class): \(.title)"' \
-| rofi -dmenu -i -p "magic" \
-| cut -f1 \
-| xargs -r hyprctl dispatch focuswindow address:
+#   | "(\(.address)) \(.class): \(.title)\u0000icon\u001f\(.class)"' \
 
+hyprctl clients -j | jq -r '.[] 
+  | select(.workspace.name == "special:shadow") 
+  | "\(.class): \(.title) (\(.address))\u0000icon\u001f\(.class)"' \
+| rofi -dmenu \
+    -i -show-icons \
+    -p "What to un-minimize?" \
+| rev \
+| cut -c2-15 \
+| rev \
+| xargs -r -I % hyprctl dispatch movetoworkspace +0,address:%
+# | xargs -r -I % rofi -e %
